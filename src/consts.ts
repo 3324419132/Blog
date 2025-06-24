44b585aecd693d713fd3deb46e46fb8495689e9f
大豆橙 <120296540+3324419132@users.noreply.github.com>
2025-06-24 22:17:10 +0800

    更新consts.ts中Github的设置

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,4 @@
 export const SITE_TITLE = '数字花园';
 export const SITE_DESCRIPTION = '代码植根于思想，生活盛开如繁花';
 export const AUTHOR_NAME = '大豆橙';
-export const GITHUB_URL = 'https://github.com/yourusername';
+export const GITHUB_URL = 'https://github.com/3324419132';
